de2c65857a8b788dda37591250c31a5e0a95b7d4
Conor D Cox <cdcox1@gmail.com>
2013-06-11 18:19:49 -0700

    xzcZXC

--- Day1Example1.py
+++ Day1Example1.py
@@ -13,7 +13,11 @@ to add two / at the end of the name.
 
 2. import numpy as np.
 
-3. Perform hist on DataIn and write this as DataOut.
+3. Perform mean on all columns and write this as DataOut.
+
+4. Do the same to all rows.
+
+5. Try a historgram.
 
 A few  things to remember: You can snag all this code and run it in your executor
 to see what it does in your variable explorer.
@@ -24,20 +28,28 @@ The error codes say which line is having a problem.
 
 When you are done, try altering my code to see how it breaks.
 
+Obviously, with list slicing we can run through each column of DataIn to build
+a mutlilayer DataOut, that being said, that is not very efficient, next lesson
+we will learn about the for loop to fix this.
+
+You will notice your data is writing as columns. This is because I am using
+itertools.izip(*DataOut)
+
 """
 import csv #Module CSV makes reading CSVs easy
 import itertools #don't worry too much about this.
 
-Directory =                #fill in your directory here
+Directory =               #fill in your directory here
 
 # Intialization code:
 InFile=Directory+"//"+"SampleData1.csv" # I added the slashes in case you failed at that
 f=open(InFile) #open the file
 DataIn=csv.reader(f)
+DataIn=list(DataIn)
 f.close() #Closing my file
-DataIn=np.array(DataIn)
+DataIn=np.array(DataIn[1:-1]).astype('float')
 ##### Your Code Goes here#####
-
+DataOut=np.histogram(DataIn)
 
 
 
@@ -47,6 +59,8 @@ DataIn=np.array(DataIn)
 
 OutFile=Directory+"//"+"OutPutData1.csv"
 g=open(OutFile,"wb")
-FlippedOut=itertools.izip(DataOut) #flipping rows and columns for easy writing
-wr=csv.writer(OutFile)
+wr=csv.writer(g)
+FlippedOut=itertools.izip(*DataOut) #flipping rows and columns for easy writing
+
 wr.writerows(FlippedOut)
+g.close()
